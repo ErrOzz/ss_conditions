5f2c323962f5ca1121c2ea8e2dfac43c631afe3d
Hermes <43679122+ErrOzz@users.noreply.github.com>
2025-04-16 21:35:03 +0500

    modify deploy.sh

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -16,16 +16,6 @@ done
 # parse rules_proxy and make PAC files
 lines=()  # Initialize an array to store transformed lines
 for line in "${filtered_lines[@]}"; do
-# mapfile -t lines < ../rules/rules_proxy
-# for ((i=0; i<${#lines[@]}; i++)); do
-    # line=${lines[i]}
-    # Skip comments and empty lines
-    # if [[ $line =~ ^#.*$ || -z $line ]]; then
-        # continue
-    # fi
-    # if [[ $line == *.* && $line != *.*.* && ${line:0:2} != '*.' ]]; then
-        # lines[i]="*.$line"
-    # fi
     if [[ $line == *.* && $line != *.*.* && ${line:0:2} != '*.' ]]; then
         lines+=("*.$line")  # Add transformed line to the array
     else
@@ -55,12 +45,7 @@ echo "[bypass_all]" > ../ss_conditions.acl
 echo "" >> ../ss_conditions.acl
 echo "[proxy_list]" >> ../ss_conditions.acl
 
-mapfile -t lines < ../rules/rules_proxy
-for line in "${lines[@]}"; do
-    # Skip comments and empty lines
-    if [[ $line =~ ^#.*$ || -z $line ]]; then
-        continue
-    fi
+for line in "${filtered_lines[@]}"; do
     if [[ $line == *.*.*.* && ${line:0:2} != '*.' ]]; then
         transformed_line="$line"
     elif [[ $line == *.*.* && ${line:0:2} != '*.' ]]; then
@@ -75,12 +60,7 @@ done
 
 # parse rules_proxy and make .CONF file
 cat ../templates/ss_conditions_template.conf > ../ss_conditions.conf
-mapfile -t lines < ../rules/rules_proxy
-for line in "${lines[@]}"; do
-    # Skip comments and empty lines
-    if [[ $line =~ ^#.*$ || -z $line ]]; then
-        continue
-    fi
+for line in "${filtered_lines[@]}"; do
     if [[ $line == *.*.*.* && ${line:0:2} != '*.' ]]; then
         transformed_line="IP-CIDR,$line,PROXY"
     else
@@ -92,12 +72,7 @@ echo "FINAL,DIRECT" >> ../ss_conditions.conf
 
 # parse rules_proxy and make .CONF file for Shadowlink
 echo -n "" > ../ss_conditions_clash.conf
-mapfile -t lines < ../rules/rules_proxy
-for line in "${lines[@]}"; do
-    # Skip comments and empty lines
-    if [[ $line =~ ^#.*$ || -z $line ]]; then
-        continue
-    fi
+for line in "${filtered_lines[@]}"; do
     if [[ $line == *.*.*.* && ${line:0:2} != '*.' ]]; then
         transformed_line="IP-CIDR,$line,PROXY"
     else
